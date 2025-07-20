f10efabef4110925fae5f38ceebd80629f9c0b9b
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2025-07-20 16:07:57 -0400

    fixing id for h2

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,7 +1,7 @@
 DROP TABLE coffee IF EXISTS;
 
 CREATE TABLE coffee  (
-    coffee_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
+    coffee_id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     brand VARCHAR(20),
     origin VARCHAR(20),
     characteristics VARCHAR(30)
