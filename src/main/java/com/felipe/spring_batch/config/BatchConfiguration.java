909f3411c5a3f7f8cba0a6cab1624c78840cecfa
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2025-07-20 16:10:37 -0400

    updating batch configuration, adding step1, job, instantiating CoffeeItemProcessor to convert items values, using constants to avoid magic strings

--- src/main/java/com/felipe/spring_batch/config/BatchConfiguration.java
+++ src/main/java/com/felipe/spring_batch/config/BatchConfiguration.java
@@ -1,6 +1,14 @@
 package com.felipe.spring_batch.config;
 
 import com.felipe.spring_batch.entity.Coffee;
+import com.felipe.spring_batch.listeners.JobCompletionNotificationListener;
+import com.felipe.spring_batch.processors.CoffeeItemProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -11,14 +19,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class BatchConfiguration {
+    public static final String INSERT_SQL = "INSERT INTO coffee (brand, origin, characteristics) VALUES (:brand, :origin, :characteristics)";
+    public static final String[] HEADER_NAMES = {"brand", "origin", "characteristics"};
     @Value("${file.input}")
     private String fileInput;
     private final String ITEM_READER_NAME = "coffeeItemReader";
+    private final String STEP1_NAME = "step1";
+    private final String IMPORT_USER_JOB_NAME = "importUserJob";
+
 
 
     @Bean
@@ -26,7 +40,7 @@ public class BatchConfiguration {
         return new FlatFileItemReaderBuilder().name(ITEM_READER_NAME)
                 .resource(new ClassPathResource(fileInput))
                 .delimited()
-                .names(new String[]{"brand", "origin", "characteristics"})
+                .names(HEADER_NAMES)
                 .fieldSetMapper(new BeanWrapperFieldSetMapper(){
                     {
                         setTargetType(Coffee.class);
@@ -38,9 +52,37 @@ public class BatchConfiguration {
     public JdbcBatchItemWriter writer(DataSource datasource) {
         return new JdbcBatchItemWriterBuilder()
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider())
-                .sql("INSERT INTO coffee (brand, origin, characteristics) VALUES (:brand, :origin, :characteristics)")
+                .sql(INSERT_SQL)
                 .dataSource(datasource)
                 .build();
+    }
+
+    @Bean
+    public Job importUserJob(JobRepository jobRepository, JobCompletionNotificationListener listener, Step step1) {
+        return new JobBuilder(IMPORT_USER_JOB_NAME, jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+                      JdbcBatchItemWriter writer) {
+        return new StepBuilder(STEP1_NAME, jobRepository)
+                .<Coffee, Coffee> chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer)
+                .build();
+
+    }
+
+
+    @Bean
+    public CoffeeItemProcessor processor() {
+        return new CoffeeItemProcessor();
     }
 
 
